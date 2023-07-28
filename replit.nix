2bfb30de10640ce69c07f60b9dd6fd3e3edf7da5
John Sheehan <jennasyseng@gmail.com>
2023-07-28 12:39:33 -0700

    Replit has become awful.

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.python39Full
-    pkgs.replitPackages.prybar-python39
+    pkgs.python310Full
+    pkgs.replitPackages.prybar-python310
     pkgs.replitPackages.stderred
+    pkgs.python39Full
     pkgs.nodejs
   ];
   env = {
@@ -15,10 +16,10 @@
       # Needed for matplotlib
       pkgs.xorg.libX11
     ];
-    PYTHONHOME = "${pkgs.python39Full}";
-    PYTHONBIN = "${pkgs.python39Full}/bin/python3.9";
+    PYTHONHOME = "${pkgs.python310Full}";
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python39}/bin/prybar-python39";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
   };
 }
