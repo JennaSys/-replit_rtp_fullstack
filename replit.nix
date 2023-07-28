a41b2268fda8b208f747ac813ef7814ead944f1f
John Sheehan <jennasyseng@gmail.com>
2023-07-28 12:13:14 -0700

    Attempt to base replit.nix off of Python3.9 instead of Python3.10

--- replit.nix
+++ replit.nix
@@ -1,10 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs
     pkgs.python39Full
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
+    pkgs.replitPackages.prybar-python39
     pkgs.replitPackages.stderred
+    pkgs.nodejs
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
@@ -16,10 +15,10 @@
       # Needed for matplotlib
       pkgs.xorg.libX11
     ];
-    PYTHONHOME = "${pkgs.python310Full}";
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    PYTHONHOME = "${pkgs.python39Full}";
+    PYTHONBIN = "${pkgs.python39Full}/bin/python3.9";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python39}/bin/prybar-python39";
   };
 }
